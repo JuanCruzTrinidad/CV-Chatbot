90e48fabd766b17591ddc4dc631a5deee673d9fb
Juan Cruz Trinidad <jtrinidad@softtek.com>
2021-04-17 20:10:22 -0300

    Add project files.

---
+++ Dialogs/RootDialog.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Bot.Builder.AI.Luis;
+using Microsoft.Bot.Builder.AI.QnA.Recognizers;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Adaptive;
+using Microsoft.Bot.Builder.Dialogs.Adaptive.Actions;
+using Microsoft.Bot.Builder.Dialogs.Adaptive.Conditions;
+using Microsoft.Bot.Builder.Dialogs.Adaptive.Recognizers;
+using Microsoft.Extensions.Configuration;
+
+namespace CV_Chatbot.Dialogs
+{
+    public class RootDialog : AdaptiveDialog
+    {
+        private readonly IConfiguration _configuration;
+
+        public RootDialog(IConfiguration configuration) : base(nameof(RootDialog))
+        {
+            _configuration = configuration;
+            //Recognizer = CreateCrossTrainedRecognizer(configuration);
+            // These steps are executed when this Adaptive Dialog begins
+
+            Triggers = new List<OnCondition>()
+                {
+                    // Add a rule to welcome user
+                    new OnConversationUpdateActivity()
+                    {
+                        Actions = WelcomeUserSteps()
+                    },
+
+                    //// Respond to user on message activity
+                    //new OnUnknownIntent()
+                    //{
+                    //    Actions = GatheUserInformation()
+                    //},
+                };
+        }
+
+        private static List<Dialog> WelcomeUserSteps()
+        {
+            return new List<Dialog>()
+            {
+                // Iterate through membersAdded list and greet user added to the conversation.
+                new Foreach()
+                {
+                    ItemsProperty = "turn.activity.membersAdded",
+                    Actions = new List<Dialog>()
+                    {
+                        // Note: Some channels send two conversation update events - one for the Bot added to the conversation and another for user.
+                        // Filter cases where the bot itself is the recipient of the message. 
+                        new SendActivity("Hola!")
+                        //new IfCondition()
+                        //{
+                        //    Condition = "$foreach.value.name != turn.activity.recipient.name",
+                        //    Actions = new List<Dialog>()
+                        //    {
+                        //        new SendActivity("Hello, I'm the multi-turn prompt bot. Please send a message to get started!")
+                        //    }
+                        //}
+                    }
+                }
+            };
+
+        }
+
+        private static Recognizer CreateCrossTrainedRecognizer(IConfiguration configuration)
+        {
+            return new CrossTrainedRecognizerSet()
+            {
+                Recognizers = new List<Recognizer>()
+                {
+                    CreateLuisRecognizer(configuration),
+                    CreateQnAMakerRecognizer(configuration)
+                }
+            };
+        }
+
+        private static Recognizer CreateQnAMakerRecognizer(IConfiguration configuration)
+        {
+            if (string.IsNullOrEmpty(configuration["qna:AddToDoDialog_en_us_qna"]) || string.IsNullOrEmpty(configuration["QnAHostName"]) || string.IsNullOrEmpty(configuration["QnAEndpointKey"]))
+            {
+                throw new Exception("NOTE: QnA Maker is not configured for AddToDoDialog. Please follow instructions in README.md to add 'qnamaker:AddToDoDialog_en_us_qna', 'QnAHostName' and 'QnAEndpointKey' to the appsettings.json file.");
+            }
+
+            return new QnAMakerRecognizer()
+            {
+                HostName = configuration["QnAHostName"],
+                EndpointKey = configuration["QnAEndpointKey"],
+                KnowledgeBaseId = configuration["qna:AddToDoDialog_en_us_qna"],
+
+                // property path that holds qna context
+                Context = "dialog.qnaContext",
+
+                // Property path where previous qna id is set. This is required to have multi-turn QnA working.
+                QnAId = "turn.qnaIdFromPrompt",
+
+                // Disable teletry logging
+                LogPersonalInformation = false,
+
+                // Enable to automatically including dialog name as meta data filter on calls to QnA Maker.
+                IncludeDialogNameInMetadata = true,
+
+                // Id needs to be QnA_<dialogName> for cross-trained recognizer to work.
+                Id = $"QnA_{nameof(RootDialog)}"
+            };
+        }
+
+        private static Recognizer CreateLuisRecognizer(IConfiguration Configuration)
+        {
+            if (string.IsNullOrEmpty(Configuration["luis:AddToDoDialog_en_us_lu"]) || string.IsNullOrEmpty(Configuration["LuisAPIKey"]) || string.IsNullOrEmpty(Configuration["LuisAPIHostName"]))
+            {
+                throw new Exception("Your AddToDoDialog's LUIS application is not configured for AddToDoDialog. Please see README.MD to set up a LUIS application.");
+            }
+            return new LuisAdaptiveRecognizer()
+            {
+                Endpoint = Configuration["LuisAPIHostName"],
+                EndpointKey = Configuration["LuisAPIKey"],
+                ApplicationId = Configuration["luis:AddToDoDialog_en_us_lu"],
+
+                // Id needs to be LUIS_<dialogName> for cross-trained recognizer to work.
+                Id = $"LUIS_{nameof(RootDialog)}"
+            };
+        }
+    }
+}
+  
---
+++ Startup.cs
@@ -0,0 +1,98 @@
+using CV_Chatbot.Dialogs;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.AI.Luis;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Adaptive;
+using Microsoft.Bot.Builder.Dialogs.Declarative;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CV_Chatbot
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers().AddNewtonsoftJson();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CV_Chatbot", Version = "v1" });
+            });
+
+            services.AddMvc();
+            
+            // Required for memory paths introduced by adaptive dialogs.
+            ComponentRegistration.Add(new DialogsComponentRegistration());
+
+            // Register declarative components
+            ComponentRegistration.Add(new DeclarativeComponentRegistration());
+
+            // Register adapive dialog components
+            ComponentRegistration.Add(new AdaptiveComponentRegistration());
+
+            // Add LUIS component
+            ComponentRegistration.Add(new LuisComponentRegistration());
+
+            // Create the Bot Framework Adapter with error handling enabled.
+            services.AddSingleton<IBotFrameworkHttpAdapter, AdapterWithErrorHandler>();
+
+            // Create the storage we'll be using for User and Conversation state. (Memory is great for testing purposes.)
+            services.AddSingleton<IStorage, MemoryStorage>();
+
+            // Create the User state. (Used in this bot's Dialog implementation.)
+            services.AddSingleton<UserState>();
+
+            // Create the Conversation state. (Used by the Dialog system itself.)
+            services.AddSingleton<ConversationState>();
+
+            // The Dialog that will be run by the bot.
+            services.AddSingleton<RootDialog>();
+
+            // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
+            services.AddSingleton<IBot, DialogBot<RootDialog>>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CV_Chatbot v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
