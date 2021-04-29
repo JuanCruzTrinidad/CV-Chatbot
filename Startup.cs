64426c6d7cfe545e8447c306602be9e2cfe04511
Juan Cruz Trinidad <jtrinidad@softtek.com>
2021-04-29 10:36:19 -0300

    use swagger

--- Startup.cs
+++ Startup.cs
@@ -85,10 +85,10 @@ namespace CV_Chatbot
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CV_Chatbot v1"));
-            }
 
+            }
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CV_Chatbot v1"));
             app.UseHttpsRedirection();
 
             app.UseRouting();
