d710aabeb2558992ef8f2a0c5f6bab912476039d
Juan Cruz Trinidad <jtrinidad@softtek.com>
2021-05-24 01:08:50 -0300

    pruebas index

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -18,31 +18,31 @@ namespace CV_Chatbot.Controllers
         [HttpGet]
         public async Task<IActionResult> PostAuthentication()
         {
-            //var secretToken = "0HoFjkIP43I.Yba-TOFzrSJyX3WoSIVl-6em34SdSwZ1jM6CkkXRNhs";// _configuration.GetValue<string>("SecretToken");
-            //HttpClient client = new HttpClient();
-            //JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+            var secretToken = "3d3BvvaxRVQ.03gWv57cdppXnyz0MvyO0kfVR29udeZGsTx44p-taiM";// _configuration.GetValue<string>("SecretToken");
+            HttpClient client = new HttpClient();
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
 
-            ////Realiza un Get con el secret token para generar uno temporal de sesion
-            //HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, $"https://webchat.botframework.com/api/tokens");
-            //request.Headers.Authorization = new AuthenticationHeaderValue("BotConnector", secretToken);
-            //var response = await client.SendAsync(request);
-            //string token = String.Empty;
-            //string jsonresult = String.Empty;
-            //if (response.IsSuccessStatusCode)
-            //{
-            //    token = await response.Content.ReadAsStringAsync();
-            //    var tokenConvert = JsonSerializer.Deserialize<DirectLineToken>(token);
-            //    //var tokenJWT = handler.ReadJwtToken(token) as JwtSecurityToken;
-            //    //var idconversacion = tokenJWT.Claims.FirstOrDefault(c => c.Type == "conv").Value;
-            //    //jsonresult = await Task.Run(() => JsonSerializer.Serialize(new { idConversation = idconversacion, token = token }));
-            //    return Ok(tokenConvert.token);
-            //}
-            //else
-            //{
-            //    var notSuccessMesagge = $"Respuesta sin exito \nFecha: {DateTime.Today} \nCodigo: {response.StatusCode} \nMensaje: {response.RequestMessage}";
-            //    //_logger.LogInformation(notSuccessMesagge);                    
-            //    return Forbid();
-            //}
+            //Realiza un Get con el secret token para generar uno temporal de sesion
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, $"https://webchat.botframework.com/api/tokens");
+            request.Headers.Authorization = new AuthenticationHeaderValue("BotConnector", secretToken);
+            var response = await client.SendAsync(request);
+            string token = String.Empty;
+            string jsonresult = String.Empty;
+            if (response.IsSuccessStatusCode)
+            {
+                token = await response.Content.ReadAsStringAsync();
+                var tokenConvert = JsonSerializer.Deserialize<DirectLineToken>(token);
+                //var tokenJWT = handler.ReadJwtToken(token) as JwtSecurityToken;
+                //var idconversacion = tokenJWT.Claims.FirstOrDefault(c => c.Type == "conv").Value;
+                //jsonresult = await Task.Run(() => JsonSerializer.Serialize(new { idConversation = idconversacion, token = token }));
+                return Ok(tokenConvert.token);
+            }
+            else
+            {
+                var notSuccessMesagge = $"Respuesta sin exito \nFecha: {DateTime.Today} \nCodigo: {response.StatusCode} \nMensaje: {response.RequestMessage}";
+                //_logger.LogInformation(notSuccessMesagge);                    
+                return Forbid();
+            }
             return Ok("Oka");
         }
     }
